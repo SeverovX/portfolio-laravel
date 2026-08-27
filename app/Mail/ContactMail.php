943c601565091c0991aa77b544a40539227ea1c3
Mony Pich San <pich.devkh@gmail.com>
2026-08-27 18:36:32 +0700

    Remove queue so emails send instantly

--- app/Mail/ContactMail.php
+++ app/Mail/ContactMail.php
@@ -2,48 +2,37 @@
 
 namespace App\Mail;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue; // Makes it run in the background
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Mail\Mailables\Address;
 
-class ContactMail extends Mailable implements ShouldQueue
+class ContactMail extends Mailable // <-- Notice we removed "implements ShouldQueue"
 {
-    use Queueable, SerializesModels;
+    use SerializesModels; // <-- Removed Queueable here too
 
     public $data;
 
-    /**
-     * Create a new message instance.
-     */
     public function __construct($data)
     {
         $this->data = $data;
     }
 
-    /**
-     * Get the message envelope.
-     */
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'New Portfolio Contact: ' . $this->data['first_name'] . ' ' . $this->data['last_name'],
+            subject: 'New Portfolio Contact from ' . $this->data['first_name'],
             replyTo: [
-                new Address($this->data['email'], $this->data['first_name']),
+                new Address($this->data['email'], $this->data['first_name'] . ' ' . $this->data['last_name']),
             ],
         );
     }
 
-    /**
-     * Get the message content definition.
-     */
     public function content(): Content
     {
         return new Content(
-            view: 'emails.contact', // We will create this view next
+            view: 'emails.contact',
         );
     }
 }
